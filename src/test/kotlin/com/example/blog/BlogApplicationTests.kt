9913f6964b6df34e551a77bc722806ff15b56d05
Daniel Moi <dmoixy@gmail.com>
2018-09-23 16:02:31 +1000

    Upgrade to use JUnit5

--- src/test/kotlin/com/example/blog/BlogApplicationTests.kt
+++ src/test/kotlin/com/example/blog/BlogApplicationTests.kt
@@ -1,11 +1,15 @@
 package com.example.blog
 
-import org.junit.Test
-import org.junit.runner.RunWith
+//import org.junit.Test
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+//import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
+// import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
+// @RunWith(SpringRunner::class)
+@ExtendWith(SpringExtension::class)
 @SpringBootTest
 class BlogApplicationTests {
 
